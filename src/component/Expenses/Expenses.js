6ca2faf33ab1ad5a7b7906f1d623a0b3b95eb64d
ncb6206 <ncb6206@gmail.com>
2023-01-08 03:26:06 +0900

    랜더링 리스트 및 조건부 content 공부중

--- src/component/Expenses/Expenses.js
+++ src/component/Expenses/Expenses.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
 import ExpensesFilter from "./ExpensesFilter";
 import "./Expenses.css";
+import ExpensesList from "./ExpensesList";
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
@@ -12,14 +12,15 @@ const Expenses = (props) => {
     setFilteredYear(selectedYear);
   };
 
+  const filteredExpenses = props.items.filter((expense) => {
+    return expense.date.getFullYear().toString() === filteredYear;
+  });
+
   return (
     <div>
       <Card className="expenses">
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem title={props.items[0].title} date={props.items[0].date} amount={props.items[0].amount} />
-        <ExpenseItem title={props.items[1].title} date={props.items[1].date} amount={props.items[1].amount} />
-        <ExpenseItem title={props.items[2].title} date={props.items[2].date} amount={props.items[2].amount} />
-        <ExpenseItem title={props.items[3].title} date={props.items[3].date} amount={props.items[3].amount} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
